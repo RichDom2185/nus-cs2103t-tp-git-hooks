113d7b9d29d8edc3321ab317e1c7fe7cd991b8ee
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2022-09-16 07:20:32 +0800

    Fix bugs + formatting

--- check-no-tabs.sh
+++ check-no-tabs.sh
@@ -5,12 +5,18 @@ ret=0
 idx=1
 files="${@:idx}"
 
-for filename in $files; do
-    if [ "$(grep -n $'\t' $filename)" ]; then
-        line=$(grep -n $'\t' $filename | cut -f1 -d:)
-        echo "[ERROR] $filename:$line: Contains tabs"
-        ret=1
-    fi
-done
-
-exit $ret
+grep -n $'\t' $files |
+awk '
+    BEGIN {
+        FS = ":"
+        OFS = ":"
+        ret = 0
+    }
+    {
+        ret = 1
+        print "[ERROR] " $1, $2, " Contains tabs."
+    }
+    END {
+        exit ret
+    }
+'
